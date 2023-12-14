dc43f97ec409c72cd9f880f4d8374c4ca5283854
Tannafama <143949496+tannafama@users.noreply.github.com>
2023-12-14 20:43:18 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,27 +1,68 @@
-    data = generate_random_data()
-def generate_random_data():
-import random
+    }
+function generateRandomData() {
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    data.forEach(item => {
+    for (let i = 0; i < 10; i++) {
+        console.log("Random Number: " + item);
+main();
+}
+}
 
-
-        print(f"Random Number: {item}")
-if __name__ == "__main__":
+    const data = generateRandomData();
+    const data = [];
+    return data;
+    });
 
     return data
-    data = [random.randint(1, 100) for _ in range(10)]
-    for item in data:
-    main()
-
-def main():    return data
-    data = generate_random_data()
 def main():
 
-def generate_random_data():
-        print(f"Random Number: {item}")
-    for item in data:
-if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
 import random
+        print(f"Random Number: {item}")
 
+    data = generate_random_data()
+def generate_random_data():
+    main()
 
+if __name__ == "__main__":
+    for item in data:    main()
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+def main():
+    for item in data:
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    return data
+if __name__ == "__main__":
+
+import random
+def main():
+    return data
+
+    for item in data:
+def generate_random_data():
+
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
 
     main()
     data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+
+import random
+    for item in data:
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+def main():
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+
